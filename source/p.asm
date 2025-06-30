1806ca8c34c27e37f5307be8d86307f8fb085013
naif19ta <juan.patino.dc@gmail.com>
2025-06-30 12:14:04 -0500

    p command

--- source/p.asm
+++ source/p.asm
@@ -1,52 +1,42 @@
 # kunx - Kernighan using unix
-# 23 Jun 2025
+# 30 Jun 2025
 # 'p' command - print file
 
 .section .rodata
 	.usage_msg: .string "p-command: p <filename>\n"
 	.usage_len: .quad   24
 
-	.unable_msg: .string "p-command: unable to open file\n"
+	.unable_msg: .string "p-command: unable to open/read file\n"
 	.unable_len: .quad   31
 
 .section .bss
-	.buffer: .zero 2048
+	.buffer: .zero 8
 
 .section .text
 
 .globl _start
+
+.include "macro.inc"
 
 _start:
 	popq	%rax
 	cmpq	$2, %rax
 	jne	.usage
 	popq	%rax
-	movq	$2, %rax
 	popq	%rdi
 	pushq	%rbp
 	movq	%rsp, %rbp
 	subq	$12, %rsp
-	xorq	%rsi, %rsi
-	xorq	%rdx, %rdx
+	RDFILE	%rdi, .unable, .buffer(%rip)	
+	movq	$1, %rax
+	movq	$1, %rdi
+	movq	.buffer(%rip), %rsi
+	movq	-12(%rbp), %rdx
 	syscall
-	cmpq	$-1, %rax
-	je	.unable
-	movl	%eax, -4(%rbp)			# file descriptor
-	movq	$8, %rax
-	movl	-4(%rbp), %edi
-	xorq	%rsi, %rsi
-	movq	$2, %rdx
-	syscall
-
-	movq	%rax, %rdi
-	movq	$60, %rax
-	syscall
-
+	jmp	.leave	
 .leave:
-	xorq	%rdi, %rdi
-	movl	-4(%rbp), %edi
-	movq	$3, %rax
-	syscall
+	UNMAP	.buffer(%rip)
+	CLSFILE
 	movq	$60, %rax
 	movq	$0, %rdi
 	syscall
@@ -68,4 +58,3 @@ _start:
 	movq	$60, %rax
 	movq	$1, %rdi
 	syscall
-
