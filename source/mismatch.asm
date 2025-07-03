37bfe41479f7594438c8b144981da2ed9d9c354f
naif19ta <juan.patino.dc@gmail.com>
2025-07-03 08:01:20 -0500

    mismatch getting word by word

--- source/mismatch.asm
+++ source/mismatch.asm
@@ -1,17 +1,21 @@
 # kunx - Kernighan using unix
-# 2 Jul 2025
-# 'mismatch' command - spot spelling errors agains a given dict
-
-.section .rodata
-	.usage_msg: .string "mismatch-command: mismatch <filename> <dict>\n"
-	.usage_len: .quad   46
-
-	.unable_msg: .string "mismatch-command: unable to open/read file\n"
-	.unable_len: .quad   43
+# 3 Jul 2025
+# 'mismatch' command - compare file's words against a provided dictionary
 
 .section .bss
-        .source: .zero 8
-	.dict: .zero 8
+	.filesrc: .zero 8
+	.dictsrc: .zero 8
+
+.section .rodata
+	.usage_msg: .string "mismatch-command: mismatch <file> <dict>\n"
+	.usage_len: .quad   41
+
+	.unable_msg: .string "mismatch-command: unable to open/read file\n"
+	.unable_len: .quad   44
+
+.section .data
+	.fileoff: .quad 0
+	.dictoff: .quad 0
 
 .section .text
 
@@ -24,37 +28,44 @@ _start:
 	cmpq	$3, %rax
 	jne	.usage
 	popq	%rax
+	# rdi: file's name
+	# r15: dict's name (temporary storage)
 	popq	%rdi
 	popq	%r15
 	pushq	%rbp
 	movq	%rsp, %rbp
-	subq	$24, %rsp
-	RDFILE	.source(%rip), -4(%rbp), -12(%rbp)
+	# -4 : file's fd
+	# -12: file's content size
+	# -16: dict's fd
+	# -24: dict's content size
+	# -32: number of lines in 'file'
+	# -40: number of lines visited in 'file'
+	# -48: current word's length
+	subq	$48, %rsp
+	RDFILE	.filesrc(%rip), -4(%rbp), -12(%rbp)
 	movq	%r15, %rdi
-	RDFILE	.dict(%rip), -16(%rbp), -24(%rbp)
-	# r8: handles source
-	# r9: handles dictionary
-	movq	.source(%rip), %r8
-	movq	.dict(%rip), %r9
+	RDFILE	.dictsrc(%rip), -16(%rbp), -24(%rbp)
+	# getting number of lines in the file
+	movq	.filesrc(%rip), %rdi
+	call	.NLines
+	movq	%rax, -32(%rbp)
+	movq	$0, -40(%rbp)
+	# Setting up buffers
+	movq	.filesrc(%rip), %rax
+	movq	%rax, (.fileoff)
 .loop:
-	movzbl	(%r8), %edi
-	cmpb	$0, %dil
+	movq	-40(%rbp), %rax
+	cmpq	-32(%rbp), %rax
 	je	.leave
 
-	call	.FindFamily
+	movq	.fileoff(%rip), %rdi
+	leaq	.fileoff(%rip), %rsi
+	call	.GetWord
+	movq	%rcx, -48(%rbp)
 
-	movq	$1, %rax
-	movq	$1, %rdi
-	movq	%r9, %rsi
-	movq	$5, %rdx
-	syscall
-
-	#jmp	.loop
-
+	incq	-40(%rbp)
+	jmp	.loop
 .leave:
-	UNMAP	.source(%rip), -12(%rbp)
-	UNMAP	.dict(%rip), -24(%rbp)
-	CLSFILE
 	movq	$60, %rax
 	movq	$0, %rdi
 	syscall
@@ -77,26 +88,51 @@ _start:
 	movq	$1, %rdi
 	syscall
 
-.FindFamily:
-.ff_loop:
-	movzbl	(%r9), %esi
-	cmpb	$0, %sil
-	je	.ff_no_fam
-	cmpb	%sil, %dil
-	jne	.ff_skip_word
-	movq	$0, %rax
-	ret
-.ff_skip_word:	
-	movzbl	(%r9), %esi
-	cmpb	$10, %sil
-	je	.ff_skiped
-	incq	%r9
-	jmp	.ff_skip_word
-.ff_skiped:
-	incq	%r9
-	jmp	.ff_loop
-.ff_no_fam:
-	movq	$-1, %rax
+# args:
+# rdi : source
+# rets:
+# rax : number of lines
+.NLines:
+	xorq	%rcx, %rcx
+.nl_loop:
+	movzbl	(%rdi), %eax
+	cmpb	$0, %al
+	je	.nl_return
+	cmpb	$'\n', %al
+	jne	.nl_resume
+	incq	%rcx
+.nl_resume:
+	incq	%rdi
+	jmp	.nl_loop
+.nl_return:
+	movq	%rcx, %rax
 	ret
 
-
+# args:
+# rdi : buffer
+# rsi : pointer to place the new offset
+# rets:
+# rax : pointer to the beg of new word
+# rcx : word's length
+.GetWord:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$8, %rsp
+	# -8: stores the current position
+	movq	%rdi, -8(%rbp)
+	xorq	%rcx, %rcx
+.gw_loop:
+	movzbl	(%rdi), %eax
+	cmpb	$0, %al
+	je	.gw_return
+	cmpb	$10, %al
+	je	.gw_return
+	incq	%rdi
+	incq	%rcx
+	incq	(%rsi)
+	jmp	.gw_loop
+.gw_return:
+	incq	(%rsi)
+	movq	-8(%rbp), %rax
+	leave
+	ret
